0db0d4d161d78ec6a1f2f01ba8d2b834170ef65a
Celie Russier <celie.russier@gmail.com>
2022-03-27 19:31:49 +0200

    last version POO-Basics-Part 2-Challenge

--- car.php
+++ car.php
@@ -5,6 +5,11 @@ require_once 'Vehicle.php';
 
 class Car extends Vehicle
 {
+    public const ALLOWED_ENERGIES = [
+        'fuel',
+        'electric',
+    ];
+    
     private string $energy;
 
     private int $energyLevel;
@@ -12,7 +17,7 @@ class Car extends Vehicle
     public function __construct(string $color, int $nbSeats, string $energy)
     {
         parent::__construct($color, $nbSeats);
-        $this->energy = $energy;
+        $this->setEnergy($energy);
     }
 
     public function getEnergy(): string
@@ -20,9 +25,12 @@ class Car extends Vehicle
         return $this->energy;
     }
 
-    public function setEnergy(string $energy): void
+    public function setEnergy(string $energy): Car
     {
+    if (in_array($energy, self::ALLOWED_ENERGIES)) {
         $this->energy = $energy;
+    }
+    return $this;
     }
 
     public function getEnergyLevel(): int
--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 require_once 'Vehicle.php';
 require_once 'bicycle.php';
 require_once 'car.php';
+require_once 'truck.php';
 
 
 $bicycle = new Bicycle('blue', 1);
@@ -11,3 +12,11 @@ var_dump($bicycle);
 $car = new Car('green', 4, 'electric');
 echo $car->forward();
 var_dump($car);
+
+var_dump(Car::ALLOWED_ENERGIES);
+
+$myTruck = new Truck('orange', 3, 'electric', 27);
+$myTruck->setCurrentStorage (12);
+var_dump($myTruck);
+echo "<br>";
+echo $myTruck->fillingRate();
